fa2ea6dee51ccbf9c7831e3621e7912a3c740f65
Smooke09 <pedrolucas1014zz@gmail.com>
2022-09-27 14:49:10 -0300

    Nested attributes

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+    def traslate_attribute(object = nil, attribute = nil)
+        if object && attribute
+        object.model.human_attribute_name(attribute)
+        else
+            "Atributo não encontrado"
+        end
+    end
 end
